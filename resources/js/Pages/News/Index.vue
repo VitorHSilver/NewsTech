46819f87eb2e80e97ee6715d940ca509104e9dd1
Vitor Henrique <vitor.silva@exata.it>
2024-06-19 14:31:09 -0300

    Component articles create

--- resources/js/Pages/News/Index.vue
+++ resources/js/Pages/News/Index.vue
@@ -8,6 +8,7 @@ import {
     MenubarShortcut,
     MenubarTrigger,
 } from "@/components/ui/menubar";
+import Articles from "./_Components/Articles.vue";
 </script>
 <template>
     <nav class="flex justify-between">
@@ -34,197 +35,24 @@ import {
         </Menubar>
     </nav>
     <!-- Main Componnent -->
-    <div class="bg-slate-100 py-4 sm:py-24">
+    <div class="bg-slate-100 py-4 sm:py-20">
         <div class="mx-auto max-w-7xl px-6 lg:px-8">
             <div class="mx-auto max-w-2xl lg:mx-0">
                 <h2
-                    class="text-3xl font-bold tracking-tight text-gray-900 sm:text-3xl"
+                    class="text-5xl font-bold tracking-tight text-gray-900 sm:text-3xl"
                 >
-                    Materias sobre tecnologia
+                    Novas Noticias
                 </h2>
-                <p class="mt-2 text-base leading-8 text-gray-600">
+                <p class="mt-2 text-xl leading-8 text-gray-600">
                     Veja as noticias mais atualizadas sobre
-                    <span class="text-sky-800 font-medium">tecnologia</span>
+                    <span class="text-sky-900/100 font-medium">tecnologia</span>
                     no dia de hoje.
                 </p>
             </div>
             <div
                 class="mx-auto mt-10 grid max-w-2x1 grid-cols-1 gap-x-8 gap-y-10 border-t border-gray-200 pt-10 sm:mt-2 sm:pt-8 lg:mx-0 lg:max-w-none lg:grid-cols-3"
             >
-                <article
-                    class="flex max-w-md flex-col items-start justify-between"
-                >
-                    <div class="flex items-center gap-x-4 text-xs">
-                        <time datetime="2020-03-16" class="text-gray-500"
-                            >Apri 29, 2024</time
-                        >
-                        <!-- Puxa a data -->
-                        <a
-                            href="#"
-                            class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-                            >BitCoin</a
-                        >
-                        <!-- Categoria -->
-                    </div>
-                    <div>
-                        <img
-                            src="img/bitcoin.jpg"
-                            alt=""
-                            class="h-300 w-100 bg-gray-50"
-                        />
-                    </div>
-                    <div class="group relative">
-                        <h3
-                            class="mt-3 text-lg font-semibold leading-5 text-gray-900 group-hover:text-gray-600"
-                        >
-                            <a href="#">
-                                <span class="absolute inset-0"></span>
-                                Maior roubo já feito em bitcoin!
-                            </a>
-                        </h3>
-                        <p
-                            class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600"
-                        >
-                            Explicando como aconteceu um dos maiores roubo de
-                            moeda virtual até o momento. Onde o Hacker levou 1
-                            milhão de BitCoins, veja detalhes sobre o caso. que
-                            está dando o que falar.
-                        </p>
-                    </div>
-                    <div class="relative mt-8 flex items-center gap-x-4">
-                        <img
-                            src="img/autor_bitcoin.jpg"
-                            alt=""
-                            class="h-10 w-10 rounded-full bg-gray-50"
-                        />
-                        <div class="text-sm leading-6">
-                            <p class="font-semibold text-gray-900">
-                                <a href="#">
-                                    <span class="absolute inset-0"></span>
-                                    Autor
-                                    <!-- Pegará o autor via PHP -->
-                                </a>
-                            </p>
-                        </div>
-                    </div>
-                </article>
-                <!-- More posts... -->
-                <article
-                    class="flex max-w-xl flex-col items-start justify-between"
-                >
-                    <div class="flex items-center gap-x-4 text-xs">
-                        <time datetime="2020-03-16" class="text-gray-500"
-                            >Jun 18, 2024</time
-                        >
-                        <a
-                            href="#"
-                            class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-                            >Games</a
-                        >
-                        <!-- Categoria -->
-                    </div>
-                    <div>
-                        <img
-                            src="/img/autor_games.jpg"
-                            alt=""
-                            class="h-300 w-100 bg-gray-50"
-                        />
-                    </div>
-                    <div class="group relative">
-                        <h3
-                            class="mt-3 text-lg font-semibold leading-5 text-gray-900 group-hover:text-gray-600"
-                        >
-                            <a href="#">
-                                <span class="absolute inset-0 max"></span>
-                                Notas de Elden Ring: Shadow of the Erdtreeee
-                            </a>
-                        </h3>
-                        <p
-                            class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600"
-                        >
-                            Hidetaka Miyazaki já havia dito que o Reino das
-                            Sombras, o novo mapa da DLC de Elden Ring, focaria
-                            em verticalidade e isso está muito enfatizado ao
-                            jogar. É possível ir muito baixo no cenário e também
-                            chegar bem alto, com o caminho se conectando de
-                            forma muito inteligente e desafiador, às vezes mais
-                            difícil que os chefes.
-                        </p>
-                    </div>
-                    <div class="relative mt-8 flex items-center gap-x-4">
-                        <img
-                            src="https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                            alt=""
-                            class="h-10 w-10 rounded-full bg-gray-50"
-                        />
-                        <div class="text-sm leading-6">
-                            <p class="font-semibold text-gray-900">
-                                <a href="#">
-                                    <span class="absolute inset-0"></span>
-                                    Autor
-                                    <!-- Pegará o autor via PHP -->
-                                </a>
-                            </p>
-                        </div>
-                    </div>
-                </article>
-                <!-- New Post -->
-                <article
-                    class="flex max-w-xl flex-col items-start justify-between"
-                >
-                    <div class="flex items-center gap-x-4 text-xs">
-                        <time datetime="2020-03-16" class="text-gray-500"
-                            >Jun 18, 2024</time
-                        >
-                        <a
-                            href="#"
-                            class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100"
-                            >Hardware</a
-                        >
-                        <!-- Categoria -->
-                    </div>
-                    <div>
-                        <img
-                            src="img/rotedor.jpg"
-                            alt=""
-                            class="h-300 w-100 bg-gray-50"
-                        />
-                    </div>
-                    <div class="group relative">
-                        <h3
-                            class="mt-3 text-lg font-semibold leading-5 text-gray-900 group-hover:text-gray-600"
-                        >
-                            <a href="#">
-                                <span class="absolute inset-0"></span>
-                                Roteadores da ASUS
-                            </a>
-                        </h3>
-                        <p
-                            class="mt-5 line-clamp-3 text-sm leading-6 text-gray-600"
-                        >
-                            Roteador da Asus apresentam problemas de
-                            vunerabilidade de segundo especialistas em
-                            segurança. A empresa já está ciente do problema e
-                            está trabalhando para corrigir o problema.
-                        </p>
-                    </div>
-                    <div class="relative mt-8 flex items-center gap-x-4">
-                        <img
-                            src="/img/autor_hardware.jpg"
-                            alt=""
-                            class="h-10 w-10 rounded-full bg-gray-50"
-                        />
-                        <div class="text-sm leading-6">
-                            <p class="font-semibold text-gray-900">
-                                <a href="#">
-                                    <span class="absolute inset-0"></span>
-                                    Autor
-                                    <!-- Pegará o autor via PHP -->
-                                </a>
-                            </p>
-                        </div>
-                    </div>
-                </article>
+                <Articles />
             </div>
         </div>
     </div>
---
+++ resources/js/Pages/News/_Components/Articles.vue
@@ -0,0 +1,71 @@
+<script setup>
+import { ref } from "vue";
+
+const articles = ref([
+    {
+        title: "Maior roubo já feito em bitcoin!",
+        category: "BitCoin",
+        date: "Apri 29, 2024",
+        author: "img/autor_bitcoin.jpg",
+        image: "img/bitcoin.jpg",
+        description:
+            "Explicando como aconteceu um dos maiores roubo de moeda virtual até o momento. Onde o Hacker levou 1 milhão de BitCoins, veja detalhes sobre o caso. que está dando o que falar.",
+        style: "bg-blue-400/50 border-blue-400 text-blue-800",
+        styleDate:
+            "bg-gray-200/50 rounded-sm p-1 border-gray-400 text-gray-800",
+    },
+    {
+        title: "Notas de Elden Ring: Shadow of the Erdtree",
+        category: "Games",
+        date: "Jun 18, 2024",
+        author: "img/autor.jpg",
+        image: "img/autor_games.jpg",
+        description:
+            "Hidetaka Miyazaki já havia dito que o Reino das Sombras, o novo mapa da DLC de Elden Ring, focaria em verticalidade e isso está muito enfatizado ao jogar. É possível ir muito baixo no cenário e também chegar bem alto, com o caminho se conectando de forma muito inteligente e desafiador, às vezes mais difícil que os chefes.",
+        style: "bg-emerald-400/50 border-emerald-400 text-emerald-800",
+    },
+    {
+        title: "Roteadores da ASUS",
+        category: "Hardware",
+        date: "Jun 18, 2024",
+        image: "img/rotedor.jpg",
+        author: "img/autor_hardware.jpg",
+        description:
+            "Roteador da Asus apresentam problemas de vunerabilidade de segundo especialistas em segurança. A empresa já está ciente do problema e está trabalhando para corrigir o problema.",
+        style: "bg-yellow-400/50 border-yellow-400 text-yellow-800",
+    },
+]);
+</script>
+<template>
+    <article v-for="article in articles" class="space-y-2">
+        <div class="flex items-center justify-between gap-4">
+            <time
+                class="text-sm bg-gray-200/50 rounded-lg p-1 border-gray-400 text-gray-800"
+                >{{ article.date }}</time
+            >
+            <div
+                class="px-2 py-1 text-xs font-medium border rounded-full"
+                :class="article.style"
+            >
+                <span>{{ article.category }}</span>
+            </div>
+        </div>
+        <img :src="article.image" alt="Imagem da notícia" class="rounded-xl" />
+        <div class="space-y-4">
+            <h3 class="text-xl truncate font-semibold text-gray-900">
+                {{ article.title }}
+            </h3>
+            <p class="mt-2 text-sm text-gray-600">
+                {{ article.description }}
+            </p>
+            <div class="relative flex items-center gap-3">
+                <img
+                    :src="article.author"
+                    alt=""
+                    class="h-10 object-cover w-10 rounded-full"
+                />
+                <span class="leading-none">Autor</span>
+            </div>
+        </div>
+    </article>
+</template>
