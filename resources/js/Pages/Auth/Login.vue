efce0e287605302ff3266c24a4a2589c33c23eaf
Vitor Henrique <vitor.silva@exata.it>
2024-07-11 14:39:00 -0300

    finishing in login page

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -29,69 +29,96 @@ const submit = () => {
 </script>
 
 <template>
-    <GuestLayout>
-        <Head title="Log in" />
+    <GuestLayout class="backgroundProject">
+        <Head title="Login" />
+        <div class="grid grid-cols-2 h-screen gap-10 bg-blue-900">
+            <div class="flex flex-col items-center justify-center">
+                <div>
+                <h1 class="text-7xl  text-white font-medium mb-10">Tech News</h1>
+                <h2 class="text-4xl font-light text-white mb-2">Seu canal de noticias</h2>
+                <h2 class="text-4xl font-light text-white ">sobre tecnologia</h2>
+            </div>
+            </div>
+            <div class="flex justify-center items-center backgroundProject">
+                <div
+                v-if="status"
+                class="mb-4 font-medium text-sm text-green-600"
+                >
+                {{ status }}
+            </div>
+            
+            <form @submit.prevent="submit" class="max-w-lg w-full ">
+                    <div>
+                        <span class="text-gray-100/40 text-sm">Voltar para home</span>
+                    </div>
+                    <div>
+                        <span class="text-gray-100 text-6xl ">Bem vindo!</span>
+                    </div>
+                    <div>
+                        <Label for="email" value="Email" />
 
-        <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
-            {{ status }}
+                        <Input
+                            id="email"
+                            type="email"
+                            class="mt-1 block w-full border-gray-400"
+                            v-model="form.email"
+                            placeholder="Email"
+                            required
+                            autofocus
+                            autocomplete="username"
+                        />
+
+                        <InputError class="mt-2" :message="form.errors.email" />
+                    </div>
+
+                    <div class="mt-4">
+                        <Label for="password" value="Password" />
+
+                        <Input
+                            id="password"
+                            type="password"
+                            class="mt-1 block w-full border-gray-400"
+                            placeholder="Senha"
+                            v-model="form.password"
+                            required
+                            autocomplete="current-password"
+                        />
+
+                        <!-- <InputError class="mt-2" :message="form.errors.password" /> -->
+                    </div>
+
+                    <div class="block mt-4">
+                        <label class="flex items-center">
+                            <Checkbox
+                                name="remember"
+                                v-model:checked="form.remember"
+                            />
+                            <span class="ms-2 text-sm text-gray-600"
+                                >Lembrar-se de mim</span
+                            >
+                        </label>
+                    </div>
+
+                    <div class="flex items-center justify-end mt-4">
+                        <Link
+                            v-if="canResetPassword"
+                            :href="route('password.request')"
+                            class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                        >
+                            Esqueceu sua senha?
+                        </Link>
+
+                        <Button
+                            class="ms-4"
+                            :class="{ 'opacity-25': form.processing }"
+                            :disabled="form.processing"
+                            variant="info"
+                        >
+                            Logar
+                        </Button>
+                    </div>
+                </form>
+            </div>
         </div>
-
-        <form @submit.prevent="submit">
-            <div>
-                <Label for="email" value="Email" />
-
-                <Input
-                    id="email"
-                    type="email"
-                    class="mt-1 block w-full"
-                    v-model="form.email"
-                    required
-                    autofocus
-                    autocomplete="username"
-                />
-
-                <InputError class="mt-2" :message="form.errors.email" />
-            </div>
-
-            <div class="mt-4">
-                <Label for="password" value="Password" />
-
-                <Input
-                    id="password"
-                    type="password"
-                    class="mt-1 block w-full"
-                    v-model="form.password"
-                    required
-                    autocomplete="current-password"
-                />
-
-                <!-- <InputError class="mt-2" :message="form.errors.password" /> -->
-            </div>
-
-            <div class="block mt-4">
-                <label class="flex items-center">
-                    <Checkbox name="remember" v-model:checked="form.remember" />
-                    <span class="ms-2 text-sm text-gray-600">Remember me</span>
-                </label>
-            </div>
-
-            <div class="flex items-center justify-end mt-4">
-                <Link
-                    v-if="canResetPassword"
-                    :href="route('password.request')"
-                    class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                >
-                    Forgot your password?
-                </Link>
-
-                <Button
-                    class="ms-4"
-                    :class="{ 'opacity-25': form.processing }"
-                    :disabled="form.processing"
-                >
-                    Log in
-                </Button>
-            </div>
-        </form>
     </GuestLayout>
 </template>
