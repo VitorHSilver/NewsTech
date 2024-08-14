2291457449ea96a68d0cdf41b397a143781ecca4
Vitor Henrique <vitor.silva@exata.it>
2024-08-14 17:37:56 -0300

    buiding forms

--- resources/js/Pages/News/Create.vue
+++ resources/js/Pages/News/Create.vue
@@ -17,6 +17,9 @@ const form = useForm({
     content: "",
     autor: props.auth.user ? props.auth.user.name : "",
     date: "",
+    time: "",
+    local: "",
+    file: "",
 });
 
 const handleSubmit = () => {
@@ -28,6 +31,7 @@ const handleSubmit = () => {
     <Head title="Publish your news" />
     <HeaderMenu />
     <section class="items-center mx-auto max-w-6xl">
+        <main>
         <div
             class="text-black flex justify-center items-center w-full rounded-lg"
         >
@@ -62,19 +66,21 @@ const handleSubmit = () => {
                             v-model="form.autor"
                         />
                     </div>
-                    <div>
+                    <div class="mt-4">
                         <label
                             class="block text-sm font-medium leading-6 text-zinc-200"
                             for="local"
                             >Local:</label
                         >
                         <Input
-                            class="w-80 text-zinc-200 bg-transparent"
+                            class="w-full text-zinc-200 bg-transparent"
                             type="text"
+                            autocomplete="street-address"
                             v-model="form.local"
                         />
                     </div>
-                    <div>
+                    <div class="mt-4 flex space-x-4">
+                        <div class=" flex flex-col ">
                         <label
                             class="block text-sm font-medium leading-6 text-zinc-200"
                             for="date"
@@ -86,9 +92,22 @@ const handleSubmit = () => {
                             v-model="form.date"
                         />
                     </div>
-                    <div class="col-span-2">
+                        <div class="flex flex-col">
                         <label
-                            class="block text-sm font-medium leading-6 text-zinc-200"
+                            class="block text-sm  font-medium leading-6 text-zinc-200"
+                            for="time"
+                            >Hora:</label
+                        >
+                        <Input
+                            class="w-20 bg-transparent text-white"
+                            type="time"
+                            v-model="form.time"
+                        />
+                    </div>
+                </div>
+                    <div class="col-span-2 mt-8">
+                        <label
+                            class="block text-sm font-medium leading-6 text-zinc-200 mb-1"
                             for="content"
                             >Resumo da informação:</label
                         >
@@ -99,11 +118,36 @@ const handleSubmit = () => {
                             />
                         </div>
                     </div>
+                    <div class="mt-4">
+                        <label
+                            class="block text-sm  font-medium leading-6 text-zinc-200"
+                            for="file"
+                            >Arquivos:</label
+                        >
+                        <Input
+                            class="w-80 text-zinc-200 mt-2"
+                            type="file"
+                            v-model="form.file"
+                        />
+                    </div>
+                </div>
+                <div>
+                    <label type="checkbox" class="text-zinc-200 px-4">
+                        <input
+                            type="checkbox"
+                            class="text-zinc-200"
+                            v-model="form.terms"
+                        />
+                        <p class="inline text-xs px-1">
+                            Autorizo a reprodução das informações acima e a utilização do meu nome nos créditos.
+                        </p>
+                    </label>
                 </div>
                 <div class="col-span-2 flex justify-end pt-2 pr-4">
-                    <Button type="submit" variant="success">Submit</Button>
+                    <Button type="submit" variant="success">Enviar</Button>
                 </div>
             </form>
         </div>
+    </main>
     </section>
 </template>
